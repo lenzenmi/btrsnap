4c594a46595fff4164f6cdc81d9e9a3a75d67edc
Mike Lenzen <lenzenmi@gmail.com>
2013-11-13 09:19:10 -0600

    Inital Commit second try

---
+++ btrsnap/btrsnap/btrsnap.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 13, 2013
+
+@author: mike
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
