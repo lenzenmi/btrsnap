a4108cfe412e252ec0940c68db7a62c4b1b67b2c
Mike Lenzen <lenzenmi@gmail.com>
2013-11-15 13:28:59 -0600

    Updated help text to be more specific.

--- btrsnap/btrsnap/btrsnap.py
+++ btrsnap/btrsnap/btrsnap.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(prog='btrsnap', description='Manage btrfs snapshots')
     parser.add_argument('--test', action='store_true')
-    parser.add_argument('dir', help='Path to root snapshot directory.')
+    parser.add_argument('dir', help='Path to root snapshot directory. This directory contains any number of subdirectories used for storing and generating snapshots. Each subdirectory must contain a single symbolic link to the btrfs subvolume you wish to create snapshots of.')
     
     args = parser.parse_args(sys.argv[1:])
     
