b063ca57574c344b4350822ab6d798705d2eea6c
Mike Lenzen <lenzenmi@gmail.com>
2013-11-15 09:26:47 -0600

    Added skeliton argparser for testing

--- btrsnap/btrsnap/btrsnap.py
+++ btrsnap/btrsnap/btrsnap.py
@@ -1,3 +1,15 @@
 '''
 This module will one day make for easy backup from btrfs to btrfs external drive
 '''
+
+if __name__ == "__main__":
+    import argparse
+    import sys
+    
+    parser = argparse.ArgumentParser(prog='btrsnap', description='Manage btrfs snapshots')
+    parser.add_argument('--test', action='store_true')
+    parser.add_argument('dir', help='Path to root snapshot directory.')
+    
+    args = parser.parse_args(sys.argv[1:])
+    
+    
