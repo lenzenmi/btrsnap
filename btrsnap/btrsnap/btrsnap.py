bb61d9da5a8a5f31a7464d74452238153c537c73
Mike Lenzen <lenzenmi@gmail.com>
2013-11-15 23:51:42 -0600

    Changed argparse options to better suit the future program

--- btrsnap/btrsnap/btrsnap.py
+++ btrsnap/btrsnap/btrsnap.py
@@ -4,12 +4,13 @@ This module will one day make for easy backup from btrfs to btrfs external drive
 
 if __name__ == "__main__":
     import argparse
-    import sys
     
     parser = argparse.ArgumentParser(prog='btrsnap', description='Manage btrfs snapshots')
-    parser.add_argument('--test', action='store_true')
-    parser.add_argument('dir', help='Path to root snapshot directory. This directory contains any number of subdirectories used for storing and generating snapshots. Each subdirectory must contain a single symbolic link to the btrfs subvolume you wish to create snapshots of.')
-    
-    args = parser.parse_args(sys.argv[1:])
+    parser.add_argument('-S', '--snapdeep', nargs=1, metavar='PATH', help='Creates a timestamped Btrfs snapshot in each subdirectory of PATH. Each subdirectory must contain a single symbolic link to the btrfs subvolume you wish to create snapshots for.')
+    parser.add_argument('-t', '--test', metavar='PATH', help='Run test suite. PATH is an empty directory on a Btrfs filesystem')
+    parser.add_argument('--version', action='version', version='%(prog) 0/0.0')
+
+    args = parser.parse_args()
+    print(args)
     
     
