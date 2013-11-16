5842f336324ed267586ae0120132a273f2eba294
Mike Lenzen <lenzenmi@gmail.com>
2013-11-16 00:19:04 -0600

    Set CLI code into a main() function.

--- btrsnap/btrsnap/btrsnap.py
+++ btrsnap/btrsnap/btrsnap.py
@@ -5,12 +5,16 @@ This module will one day make for easy backup from btrfs to btrfs external drive
 if __name__ == "__main__":
     import argparse
     
-    parser = argparse.ArgumentParser(prog='btrsnap', description='Manage btrfs snapshots')
-    parser.add_argument('-S', '--snapdeep', nargs=1, metavar='PATH', help='Creates a timestamped Btrfs snapshot in each subdirectory of PATH. Each subdirectory must contain a single symbolic link to the btrfs subvolume you wish to create snapshots for.')
-    parser.add_argument('-t', '--test', metavar='PATH', help='Run test suite. PATH is an empty directory on a Btrfs filesystem')
-    parser.add_argument('--version', action='version', version='%(prog) 0/0.0')
-
-    args = parser.parse_args()
-    print(args)
+    def main():
+        '''
+        Command Line Interface.
+        '''
+        parser = argparse.ArgumentParser(prog='btrsnap', description='Manage btrfs snapshots')
+        parser.add_argument('-S', '--snapdeep', nargs=1, metavar='PATH', help='Creates a timestamped Btrfs snapshot in each subdirectory of PATH. Each subdirectory must contain a single symbolic link to the btrfs subvolume you wish to create snapshots for.')
+        parser.add_argument('-t', '--test', metavar='PATH', help='Run test suite. PATH is an empty directory on a Btrfs filesystem')
+        parser.add_argument('--version', action='version', version='%(prog) 0/0.0')
+        args = parser.parse_args()
+        print(args)
     
-    
+    #start the program
+    main()
