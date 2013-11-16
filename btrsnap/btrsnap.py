8f0665f3dc6e065353b92e8da758c7f33aeb6e70
Mike Lenzen <lenzenmi@gmail.com>
2013-11-16 10:48:08 -0600

    Changed project layout and added Path class along with a few tests

--- btrsnap/btrsnap.py
+++ btrsnap/btrsnap.py
@@ -2,19 +2,35 @@
 This module will one day make for easy backup from btrfs to btrfs external drive
 '''
 
+import os
+
+class PathError(Exception):
+    pass
+
+class Path:
+    def __init__(self, path):
+        if os.path.isdir(os.path.expanduser(path)):
+            self.path = os.path.abspath(os.path.expanduser(path))
+        else:
+            raise PathError('Path not valid')
+
 if __name__ == "__main__":
-    import argparse
     
     def main():
         '''
         Command Line Interface.
         '''
+        
+        import argparse
+
         parser = argparse.ArgumentParser(prog='btrsnap', description='Manage btrfs snapshots')
         parser.add_argument('-S', '--snapdeep', nargs=1, metavar='PATH', help='Creates a timestamped Btrfs snapshot in each subdirectory of PATH. Each subdirectory must contain a single symbolic link to the btrfs subvolume you wish to create snapshots for.')
-        parser.add_argument('-t', '--test', metavar='PATH', help='Run test suite. PATH is an empty directory on a Btrfs filesystem')
         parser.add_argument('--version', action='version', version='%(prog) 0/0.0')
         args = parser.parse_args()
+        
+        #debug output
         print(args)
-    
+        
+           
     #start the program
     main()
---
+++ btrsnap/tests.py
@@ -0,0 +1,54 @@
+'''
+Created on Nov 15, 2013
+
+@author: mike
+'''
+import unittest
+import os
+import shutil
+
+import btrsnap
+
+
+class Test_Path_Class(unittest.TestCase):
+
+    test_dir = ''
+
+    def setUp(self):
+        try:
+            environ_path = os.path.expanduser(os.environ['BTRSNAP_TEST_DIR'])
+            self.test_dir = os.path.join(os.path.realpath(os.path.abspath(environ_path)), 'btrsnap_test_dir')
+            
+        except Exception:
+            print('you must assign the environment variable BTRSNAP_TEST_DIR=PATH\n'
+                  + 'Where path = a path on a btrfs filesystem')
+        
+        test_dir = self.test_dir
+        if os.path.isdir(test_dir):
+            print('{} exists. Please remove it and re-run tests'.format(test_dir))
+            exit(1)
+        os.mkdir(test_dir)
+        print(test_dir)
+                
+    def tearDown(self):
+        test_dir = self.test_dir
+        shutil.rmtree(test_dir)
+
+    def test_path_valid_path(self):
+        test_dir = os.path.join(self.test_dir, 'empty_path')
+        os.mkdir(test_dir)
+        path = btrsnap.Path(test_dir)
+        self.assertEqual(test_dir, path.path)
+    
+    def test_path_invalid_path(self):
+        test_dir = os.path.join(self.test_dir, 'not_a_dir')
+        self.assertRaises(btrsnap.PathError, btrsnap.Path, test_dir)
+    
+    def test_path_userexpansion(self):
+        path = btrsnap.Path('~')
+        self.assertEqual(path.path, os.path.expanduser('~'))
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
